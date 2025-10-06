c1e70ceee2ee6a0c3c175609f0b89fc43d06376d
lovasoa4 <hei.lovasoa.3@gmail.com>
2025-10-06 12:26:00 +0300

    creation table et exemple de donner insert

--- base.sql
+++ base.sql
@@ -10,5 +10,20 @@ create table transaction(
     type varchar(50) not null,
     date_transaction datetime DEFAULT CURRENT_TIMESTAMP,
     montant decimal not null,
-    description text
+    description text,
+    id_user int,
+    foreign key (id_user) references users(id)
 )
+
+-- Insertion des utilisateurs
+INSERT INTO users (nom, email, mdp) VALUES
+('Lova', 'lova@gmail.com', '12345'),
+('Tiana', 'tiana@gmail.com', 'abcde');
+
+-- Insertion des transactions
+INSERT INTO transaction (type, montant, description, id_user) VALUES
+('crédit', 250000, 'Salaire du mois', 1),
+('débit', 50000, 'Achat de provisions', 1),
+('débit', 30000, 'Achat de carburant', 1),
+('crédit', 150000, 'Vente d’objets personnels', 2),
+('débit', 70000, 'Paiement facture électricité', 2);
